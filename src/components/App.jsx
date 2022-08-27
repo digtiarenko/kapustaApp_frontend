9c52b8e8f4d78ef23797a98aa5f06be50d8bf0e3
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2022-08-27 18:16:07 +0300

    add start files

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>KAPUSTA LOGO</p>
+      </header>
+      <main>
+        <p> Our app should be here </p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/redux/store.js
@@ -0,0 +1,20 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+export const store = configureStore({
+  reducer: {},
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  // devTools: process.env.NODE_ENV !== 'production',
+});
