ba8b6f2c96e45aa17814ecb4bcac1c55d482779a
Arthur Habrielyan <zkdzkd227@gmail.com>
2022-08-29 17:40:43 +0300

    finished style of component Summary

--- src/Views/ReportsPage/ReportPage.jsx
+++ src/Views/ReportsPage/ReportPage.jsx
@@ -1,7 +1,11 @@
+import { Summary } from './Summary/Summary';
+import s from './reportPage.module.css';
+
 export default function ReportPage() {
   return (
     <>
-      <section>
+      <section className={s.section}>
+        <Summary></Summary>
         <h1>Page for working with the reports</h1>
         <p>Welcome to the best resource for see how much you earn and spend</p>
       </section>
