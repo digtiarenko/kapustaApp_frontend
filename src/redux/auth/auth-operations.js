162e1117fbe728682851f37a4c03a5c9085fddb8
Valentyn Digtiarenko <baggins999@gmail.com>
2022-08-31 22:32:48 +0300

    second commit

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = `http://localhost:5000/api`;
+axios.defaults.baseURL = `https://kapustaapplication.herokuapp.com/api`;
 
 const token = {
   set(token) {
