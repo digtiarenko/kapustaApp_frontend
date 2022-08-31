386f78c65007276a63759bbe7dcb46d9ace6c845
anasty223 <86348493+anasty223@users.noreply.github.com>
2022-08-31 21:52:23 +0300

    add btn

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -1,10 +1,13 @@
 const getIsLoggedIn = state => state.auth.isLoggedIn;
-
+const getUserName = state => state.auth.user.name;
 const getIsRefreshing = state => state.auth.isRefreshing;
+const getUserEmail = state => state.auth.user.email;
 
 const authSelectors = {
   getIsLoggedIn,
   getIsRefreshing,
+  getUserName,
+  getUserEmail,
 };
 
 export default authSelectors;
