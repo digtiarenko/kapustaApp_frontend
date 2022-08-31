c6bb32a37ce9c37d049b162117e74d4ed3222d75
Vladimyr Kulagin <vladimyrkulagin@gmail.com>
2022-08-31 21:36:31 +0300

    added income page, downloaded new icons for expenses and income

--- src/Views/ReportsPage/Income/Income.jsx
+++ src/Views/ReportsPage/Income/Income.jsx
@@ -1,12 +1,12 @@
 import s from './Income.module.css';
 import SliderIncomeExpenses from '../../../modules/reports/components/SliderIncomeExpenses';
-import ExpensesList from '../../../modules/reports/components/ExpensesList';
+import IncomeList from '../../../modules/reports/components/IncomeList';
 
 export default function Income() {
   return (
     <div className={s.wrapper}>
       <SliderIncomeExpenses text="income" link="expenses" />
-      <ExpensesList />
+      <IncomeList />
     </div>
   );
 }
--- src/modules/reports/components/IncomeList/IncomeList.js
+++ src/modules/reports/components/IncomeList/IncomeList.js
@@ -1,12 +1,12 @@
-import s from './ExpensesList.module.css';
+import s from './IncomeList.module.css';
 import ExpensesItem from '../ExpensesItem';
-import icons from '../../../../images/icons/expenses/categoryIcons';
+import icons from '../../../../images/icons/income/icons';
 
-export default function ExpensesList() {
+export default function IncomeList() {
   return (
     <ul className={s.list}>
       {icons.map((icon, idx, arr) => (
-        <ExpensesItem key={idx} category="alcohol" sum="200">
+        <ExpensesItem key={idx} category="income" sum="40000">
           <img src={icon} alt="icon" className={s.icon} />
         </ExpensesItem>
       ))}
