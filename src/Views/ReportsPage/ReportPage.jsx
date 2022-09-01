c11718339fdf993bfca57909a330d6e3463a845a
Vladimyr Kulagin <vladimyrkulagin@gmail.com>
2022-09-01 16:59:13 +0300

    styles

--- src/Views/ReportsPage/ReportPage.jsx
+++ src/Views/ReportsPage/ReportPage.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from 'react-router-dom';
 import { Summary } from './Summary/Summary';
-import Expenses from './Expenses/Expenses';
 import s from './reportPage.module.css';
 
 export default function ReportPage() {
@@ -8,7 +7,6 @@ export default function ReportPage() {
     <>
       <section className={s.section}>
         <Summary></Summary>
-        {/* <Expenses /> */}
         <Outlet />
         <h1>Page for working with the reports</h1>
         <p>Welcome to the best resource for see how much you earn and spend</p>
