55c5c2c46e8904b50e4f539f1c3a56fbbdd809da
anasty223 <86348493+anasty223@users.noreply.github.com>
2022-09-01 22:18:10 +0300

    add name header

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -1,7 +1,7 @@
 const getIsLoggedIn = state => state.auth.isLoggedIn;
 const getUserName = state => state.auth.user.name;
 
-const getUserEmail = state => state.auth.user.email;
+const getUserEmail = state => state.auth.user.user.email;
 
 const getIsRefreshing = state => state.auth.isRefreshing;
 
