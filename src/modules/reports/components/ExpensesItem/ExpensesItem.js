a99d0d1340474e506d779333bba772b5228035bf
Vladimyr Kulagin <vladimyrkulagin@gmail.com>
2022-09-01 23:06:51 +0300

    expenses css

--- src/modules/reports/components/ExpensesItem/ExpensesItem.js
+++ src/modules/reports/components/ExpensesItem/ExpensesItem.js
@@ -4,7 +4,7 @@ import s from './ExpensesItem.module.css';
 export default function ExpensesItem({ category, sum, children }) {
   return (
     <Link to={`/reports/expenses/${category.toLowerCase()}`} className={s.link}>
-      <p>{sum}</p>
+      <p className={s.sum}>{sum}</p>
       {children}
       <p className={s.category}>{category.toLowerCase()}</p>
     </Link>
