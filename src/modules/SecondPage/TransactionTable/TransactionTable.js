1090bf6d1456a238be352e124d0021bd34ea2e27
GalSanyi <86233181+GalSanyi@users.noreply.github.com>
2022-09-03 17:09:09 +0200

    sacond-page

---
+++ src/modules/SecondPage/TransactionTable/TransactionTable.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import s from './TransactionTable.module.css';
+function TransactionTable() {
+  return (
+    <div>
+      <table className={s.table}>
+        <thead>
+          <tr>
+            <th>дата</th>
+            <th>описание</th>
+            <th>категория</th>
+            <th>сумма</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default TransactionTable;
