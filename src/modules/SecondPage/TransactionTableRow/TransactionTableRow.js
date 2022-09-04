adbb637938a14f4cda2da8b38dfbd8688d366b18
Marharyta Bosak <aboimova.rita@gmail.com>
2022-09-04 11:34:18 +0300

    add sum style

--- src/modules/SecondPage/TransactionTableRow/TransactionTableRow.js
+++ src/modules/SecondPage/TransactionTableRow/TransactionTableRow.js
@@ -2,13 +2,34 @@ import React from 'react';
 import { ReactComponent as Delete } from '../../../images/icons/delete.svg';
 import s from './TransactionTableRow.module.css';
 
-function TransactionTableRow({ date, description, category, sum, onDelete }) {
+const getSumTypeStyle = type => {
+  switch (type) {
+    case 'income':
+      return {
+        color: '#407946',
+      };
+    case 'expenses':
+      return {
+        color: '#E7192E',
+      };
+    default:
+      return {};
+  }
+};
+
+function TransactionTableRow({ transactionData, onDelete }) {
+  const { type, date, description, category, sum } = transactionData;
+
+  const sumStyle = getSumTypeStyle(type);
+
   return (
     <tr className={s.tableRow}>
       <td>{date}</td>
       <td>{description}</td>
       <td>{category}</td>
-      <td className={s.sum}>{sum}</td>
+      <td className={s.sum} style={sumStyle}>
+        {sum}
+      </td>
       <td>
         <button type="button" className={s.button} onClick={onDelete}>
           <Delete className={s.svg} />
