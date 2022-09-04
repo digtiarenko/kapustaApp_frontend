fc1e27a98bd52da8a8cd9c44efc66e1b95f9f259
Vladimyr Kulagin <vladimyrkulagin@gmail.com>
2022-09-05 00:07:45 +0300

    fixed styles, added income from backend

--- src/modules/reports/components/ExpensesList/ExpensesList.js
+++ src/modules/reports/components/ExpensesList/ExpensesList.js
@@ -1,10 +1,20 @@
-import { useSelector } from 'react-redux';
-import { getCategoriesList } from '../../../../redux/categories/categoriesSelectors';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
 import s from './ExpensesList.module.css';
 import ExpensesItem from '../ExpensesItem';
 import icons from '../../../../images/icons/category/expenses/categoryIcons';
 
+import categoriesOperations from 'redux/categories/categoriesOperations';
+import { getCategoriesList } from '../../../../redux/categories/categoriesSelectors';
+
 export default function ExpensesList() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(categoriesOperations.getCategoriesList());
+  }, [dispatch]);
+
   const categoriesList = useSelector(getCategoriesList);
   let expensesList;
   if (categoriesList) {
@@ -22,7 +32,16 @@ export default function ExpensesList() {
           <div className={s.item} key={item.name}>
             <ExpensesItem category={item.name} sum="200">
               <img
-                src={icons[`${item.name.split(', ').join('').toLowerCase()}`]}
+                src={
+                  icons[
+                    `${item.name
+                      .split(', ')
+                      .join('')
+                      .split(' ')
+                      .join('')
+                      .toLowerCase()}`
+                  ]
+                }
                 alt={`${item.name}`}
                 className={s.icon}
               />
--- src/modules/reports/components/IncomeList/IncomeList.js
+++ src/modules/reports/components/IncomeList/IncomeList.js
@@ -1,17 +1,55 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
 import s from './IncomeList.module.css';
 import ExpensesItem from '../ExpensesItem';
 import icons from '../../../../images/icons/category/income/icons';
 
+import categoriesOperations from 'redux/categories/categoriesOperations';
+import { getCategoriesList } from '../../../../redux/categories/categoriesSelectors';
+
 export default function IncomeList() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(categoriesOperations.getCategoriesList());
+  }, [dispatch]);
+
+  const categoriesList = useSelector(getCategoriesList);
+  let incomeList;
+  if (categoriesList) {
+    incomeList = categoriesList.filter(value => value.type === 'income');
+    console.log(incomeList);
+    console.log(icons['alcohol']);
+    console.log(
+      incomeList[0].name.split(', ').join('').split(' ').join('').toLowerCase()
+    );
+    console.log(incomeList[0].category_id);
+  }
+
   return (
     <ul className={s.list}>
-      {icons.map((icon, idx, arr) => (
-        <div className={s.item} key={idx}>
-          <ExpensesItem category="income" sum="40000">
-            <img src={icon} alt="icon" className={s.icon} />
-          </ExpensesItem>
-        </div>
-      ))}
+      {incomeList &&
+        incomeList.map(item => (
+          <div className={s.item} key={item.name}>
+            <ExpensesItem category={item.name} sum="40000">
+              <img
+                src={
+                  icons[
+                    `${item.name
+                      .split(', ')
+                      .join('')
+                      .split(' ')
+                      .join('')
+                      .toLowerCase()}`
+                  ]
+                }
+                alt={`${item.name}`}
+                className={s.icon}
+              />
+            </ExpensesItem>
+          </div>
+        ))}
     </ul>
   );
 }
