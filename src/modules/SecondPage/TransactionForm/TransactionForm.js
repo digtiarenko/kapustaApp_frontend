d667d50f440c841f4174b4f8b5fe102150182277
GalSanyi <86233181+GalSanyi@users.noreply.github.com>
2022-09-04 16:35:54 +0200

    adaptive

--- src/modules/SecondPage/TransactionForm/TransactionForm.js
+++ src/modules/SecondPage/TransactionForm/TransactionForm.js
@@ -4,38 +4,39 @@ import Datepicker from 'modules/DatePicker/Datepicker';
 import Dropdown from 'modules/dropDownCategories/Dropdown';
 function TransactionForm() {
   return (
-    <div>
+    <div className={s.containerForm}>
       <form className={s.form}>
-        <div className={s.calendarWrapper}>
-          <Datepicker />
-        </div>
+        <div className={s.formControl}>
+          <div className={s.calendarWrapper}>
+            <Datepicker />
+          </div>
 
-        <input
-          className={s.product}
-          type="text"
-          placeholder="Product description"
-        />
+          <input
+            className={s.product}
+            type="text"
+            placeholder="Product description"
+          />
 
-        <div className={s.category}>
-          {/* <p className={s.text}>Product category</p> */}
-          <Dropdown />
-        </div>
+          <div className={s.category}>
+            {/* <p className={s.text}>Product category</p> */}
+            <Dropdown />
+          </div>
 
-        <div className={s.calc}>
-          <input className={s.sammy} type="text" placeholder="0,00" />
-          <Calc className={s.svg} width="20" height="20" />
-        </div>
-
-        <div className={s.btnWrapper}>
-          <button type="submit" className={s.btn}>
-            input
-          </button>
-
-          <button type="button" className={s.btn}>
-            clear
-          </button>
+          <div className={s.calc}>
+            <input className={s.sammy} type="text" placeholder="0,00" />
+            <Calc className={s.svg} width="20" height="20" />
+          </div>
         </div>
       </form>
+      <div className={s.btnWrapper}>
+        <button type="submit" className={s.btn}>
+          input
+        </button>
+
+        <button type="button" className={s.btn}>
+          clear
+        </button>
+      </div>
     </div>
   );
 }
