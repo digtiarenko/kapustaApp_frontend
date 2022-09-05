4f0eae778f3b1b353d6bed18aa254fbd1987ae56
Arthur Habrielyan <zkdzkd227@gmail.com>
2022-09-06 01:14:18 +0300

    clean useEffect

--- src/modules/SecondPage/Page/Page.js
+++ src/modules/SecondPage/Page/Page.js
@@ -21,6 +21,7 @@ export default function Page({ balance, setBalance }) {
     ) {
       navigation('/home/expenses');
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
