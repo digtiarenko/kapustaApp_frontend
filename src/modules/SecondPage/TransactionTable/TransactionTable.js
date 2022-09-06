2677593d9ed4479802421bf1c60ca29877e8b271
Arthur Habrielyan <zkdzkd227@gmail.com>
2022-09-07 01:26:26 +0300

    Revert "commit"
    This reverts commit 2ae300a730ccdbf6cabb5897f1a97a2431029cce.

---
+++ src/modules/SecondPage/TransactionTable/TransactionTable.js
@@ -0,0 +1,61 @@
+import React, { useEffect } from 'react';
+import s from './TransactionTable.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { getTransactionsByTypeAndDate } from '../../../redux/transactions/transactionsOperations';
+import { getTransactions } from '../../../redux/transactions/transactionsSelectors';
+import { TransactionTableRow } from '../TransactionTableRow/TransactionTableRow';
+
+const TransactionTable = ({ date, type }) => {
+  const transactions = useSelector(getTransactions);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    transactions &&
+      type &&
+      dispatch(
+        getTransactionsByTypeAndDate({
+          type,
+          date,
+          page: '1',
+          limit: '9',
+        })
+      );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [date, type]);
+
+  return (
+    <div className={s.container}>
+      <table className={s.table}>
+        <thead>
+          <tr>
+            <th>дата</th>
+            <th>описание</th>
+            <th>категория</th>
+            <th>сумма</th>
+            <th></th>
+          </tr>
+        </thead>
+      </table>
+
+      <div className={s.scroll}>
+        <table className={s.secondTable}>
+          <tbody>
+            {transactions &&
+              transactions.map(transaction => (
+                <TransactionTableRow
+                  key={transaction._id}
+                  id={transaction._id}
+                  date={transaction.date}
+                  description={transaction.description}
+                  type={transaction.type}
+                  value={transaction.value}
+                  category={transaction.category.name}
+                />
+              ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default TransactionTable;
