0fa8e5520d2fcad37279a81006e32124c934cb4a
Valentyn Digtiarenko <69214792+digtiarenko@users.noreply.github.com>
2022-09-09 17:45:01 +0300

    Revert "Bugfix/home page mobile"

--- src/modules/SecondPage/TransactionTable/TransactionTable.js
+++ src/modules/SecondPage/TransactionTable/TransactionTable.js
@@ -1,41 +1,26 @@
 import React, { useEffect } from 'react';
 import s from './TransactionTable.module.css';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  getTransactionsByDate,
-  getTransactionsByTypeAndDate,
-} from '../../../redux/transactions/transactionsOperations';
+import { getTransactionsByTypeAndDate } from '../../../redux/transactions/transactionsOperations';
 import { getTransactions } from '../../../redux/transactions/transactionsSelectors';
 import { TransactionTableRow } from '../TransactionTableRow/TransactionTableRow';
 import EmptyRows from './EmptyRows';
 
 const TransactionTable = ({ date, type }) => {
   const transactions = useSelector(getTransactions);
+  console.log(type);
   const dispatch = useDispatch();
   useEffect(() => {
-    if (type === 'expenses-income') {
-      transactions &&
-        dispatch(
-          getTransactionsByDate({
-            date,
-          })
-        );
-    } else {
-      transactions &&
-        type &&
-        dispatch(
-          getTransactionsByTypeAndDate({
-            date,
-            type,
-          })
-        );
-    }
+    transactions &&
+      type &&
+      dispatch(
+        getTransactionsByTypeAndDate({
+          date,
+          type,
+        })
+      );
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [date, type]);
-
-  const sortTransactions = [...transactions].sort((a, b) => {
-    return b.createdAt.localeCompare(a.createdAt);
-  });
 
   return (
     <table className={s.table}>
@@ -49,8 +34,8 @@ const TransactionTable = ({ date, type }) => {
         </tr>
       </thead>
       <tbody className={s.tableBody}>
-        {sortTransactions &&
-          sortTransactions.map(transaction => (
+        {transactions &&
+          transactions.map(transaction => (
             <TransactionTableRow
               key={transaction._id}
               id={transaction._id}
--- src/modules/reports/components/ReportChart/ReportChart.jsx
+++ src/modules/reports/components/ReportChart/ReportChart.jsx
@@ -1,42 +1,33 @@
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { useMediaQuery } from 'react-responsive';
 import { getDataByMonth } from 'redux/reports/reportsSelectors';
 import ReportChartMobile from './ReportChartMobile';
 import ReportChartDesktop from './ReportChartDesktop';
 
 const ReportChart = () => {
-  const notMobile = useMediaQuery({ minWidth: 768 });
-  const isDesktop = useMediaQuery({ minWidth: 1280 });
+  const [screenWidth, setScreenWidth] = useState(window.screen.width);
+  const handleScreenResize = () => setScreenWidth(window.screen.width);
   const { categoryId } = useParams();
 
   const reportFullMonth = useSelector(getDataByMonth);
 
-  const arrOfTypes =
+  useEffect(() => {
+    window.addEventListener('resize', handleScreenResize);
+    return () => window.removeEventListener('resize', handleScreenResize);
+  }, []);
+
+  const data =
     reportFullMonth[0] && reportFullMonth[0].date
-      ? reportFullMonth[0].arrOfTypes
-      : [];
-
-  const arrOfCategories = arrOfTypes.length
-    ? arrOfTypes.reduce((acc, item) => {
-        return acc.concat(item.arrOfCategories);
-      }, [])
-    : [];
-
-  const arrOfTransaction = arrOfCategories.length
-    ? arrOfCategories.reduce((acc, item) => {
-        if (item.category._id === categoryId) {
-          return acc.concat(item.arrOfTransactions);
-        }
-        return acc;
-      }, [])
-    : [];
-  const data = arrOfTransaction;
+      ? reportFullMonth[0].arrOfTypes[0].arrOfCategories
+          .concat(reportFullMonth[0].arrOfTypes[1].arrOfCategories)
+          .filter(item => item.category._id === categoryId)[0].arrOfTransactions
+      : null;
   return (
     <>
-      {notMobile
-        ? data && <ReportChartDesktop isDesktop={isDesktop} data={data} />
-        : data && <ReportChartMobile data={data} />}
+      {screenWidth < 768
+        ? data && <ReportChartMobile data={data} />
+        : data && <ReportChartDesktop screen={screenWidth} data={data} />}
     </>
   );
 };
