72f83425a5bd0bce36a96724879af3d11cbc27b1
Volodymyr Baraban <barabanv2003@gmail.com>
2022-09-09 10:45:27 +0300

    thousandSeparator

--- src/modules/balance/components/Balance/Balance.js
+++ src/modules/balance/components/Balance/Balance.js
@@ -53,6 +53,7 @@ export default function Balance({ type }) {
           <NumberFormat
             className={type === 'home' ? s.input : s.reportInputStyle}
             fixedDecimalScale={'true'}
+            thousandSeparator=" "
             decimalScale={'2'}
             value={balance}
             placeholder={'00.00 UAH'}
