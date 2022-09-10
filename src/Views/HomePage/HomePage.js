44a6acb162b19d065ea4d00b608adbc5c2cf7d38
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2022-09-10 14:42:30 +0300

    fix transaction table and add new date picker

--- src/Views/HomePage/HomePage.js
+++ src/Views/HomePage/HomePage.js
@@ -3,7 +3,7 @@ import ReportsLink from '../../modules/reports/components/ReportsLink';
 import Container from 'modules/Container';
 import s from './HomePage.module.css';
 import IncomeExpense from '../../modules/SecondPage/IncomeExpense/IncomeExpense';
-import { useEffect, useState } from 'react';
+import { forwardRef, useEffect, useState } from 'react';
 import TransactionTable from '../../modules/SecondPage/TransactionTable/TransactionTable';
 import TransactionForm from '../../modules/SecondPage/TransactionForm/TransactionForm';
 import moment from 'moment';
@@ -12,6 +12,31 @@ import { ReactComponent as CalendarLogo } from 'images/icons/calendar.svg';
 
 import { useMediaQuery } from 'react-responsive';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import {
+  getTransactionsByDate,
+  getTransactionsByTypeAndDate,
+} from 'redux/transactions/transactionsOperations';
+import DatePicker from 'react-datepicker';
+
+import 'react-datepicker/dist/react-datepicker.css';
+
+const DatePickerCustomInput = forwardRef(({ value, onClick }, ref) => (
+  <button className={s.dateButton} onClick={onClick} ref={ref}>
+    <CalendarLogo className={s.calendarIcon} />
+    <span className={s.dateButtonText}>{value}</span>
+  </button>
+));
+
+const formatDate = date => {
+  const year = date.getFullYear();
+  const month =
+    date.getMonth() + 1 < 10
+      ? '0' + (date.getMonth() + 1)
+      : date.getMonth() + 1;
+  const day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+  return year + '-' + month + '-' + day;
+};
 
 export default function HomePage() {
   const notMobile = useMediaQuery({ minWidth: 768 });
@@ -19,19 +44,32 @@ export default function HomePage() {
 
   const location = useLocation();
   const navigation = useNavigate();
+  const dispatch = useDispatch();
   const [date, setDate] = useState(moment().format('YYYY-MM-DD'));
-  const [type, setType] = useState('');
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [type, setType] = useState('expenses');
 
   useEffect(() => {
     if (notMobile) {
+      if (type === 'expenses' || type === 'income')
+        dispatch(
+          getTransactionsByTypeAndDate({
+            date,
+            type,
+          })
+        );
       navigation('/home/expenses');
     }
     if (isMobile) {
       setType('expenses-income');
+      dispatch(
+        getTransactionsByDate({
+          date: formatDate(selectedDate),
+        })
+      );
       navigation('/home');
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isMobile, notMobile]);
+  }, [date, dispatch, isMobile, navigation, notMobile, selectedDate, type]);
 
   useEffect(() => {
     if (location.pathname === '/home/expenses') {
@@ -57,50 +95,53 @@ export default function HomePage() {
                   <div className={s.balanceBlock}>
                     <ReportsLink />
                     <Balance type="home" />
-                  </div>
-                </div>
-                <div className={s.buttonCont}>
-                  <div className={s.ab}>{notMobile && <IncomeExpense />}</div>
-                  <div className={s.container}>
-                    {notMobile ? (
-                      <>
-                        <TransactionForm
-                          date={date}
-                          setDate={setDate}
-                          type={type}
-                        />
-                      </>
-                    ) : (
+                    {isMobile && (
                       <div className={s.dateWrapper}>
-                        <input
-                          aria-label="Date"
-                          name="date"
-                          onChange={e => setDate(e.target.value)}
-                          type="date"
-                          className={s.datePicker}
-                          value={date}
+                        <DatePicker
+                          dateFormat="dd.MM.yyyy"
+                          selected={selectedDate}
+                          onChange={date => setSelectedDate(date)}
+                          customInput={<DatePickerCustomInput />}
                         />
-                        <CalendarLogo className={s.calendarIcon} />
                       </div>
                     )}
-                    <div className={s.overlaySummery}>
-                      <TransactionTable date={date} type={type} />
-                      <div>
-                        <Summary />
+                  </div>
+                </div>
+                {notMobile ? (
+                  <div className={s.buttonCont}>
+                    <div className={s.ab}>
+                      <IncomeExpense />
+                    </div>
+                    <div className={s.container}>
+                      <TransactionForm
+                        date={date}
+                        setDate={setDate}
+                        type={type}
+                      />
+                      <div className={s.overlaySummery}>
+                        <TransactionTable date={date} type={type} />
+                        <div>
+                          <Summary />
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
+                ) : (
+                  <TransactionTable
+                    date={formatDate(selectedDate)}
+                    type={type}
+                  />
+                )}
               </div>
             </div>
           </section>
+          {isMobile && (
+            <div className={s.buttonCont}>
+              <IncomeExpense />
+            </div>
+          )}
         </Container>
       </div>
-      {isMobile && (
-        <div className={s.buttonCont}>
-          <IncomeExpense />
-        </div>
-      )}
     </div>
   );
 }
